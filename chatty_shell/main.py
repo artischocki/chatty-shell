9e69ab050a389eb0e11de9a64d10b88a52258272
Artur Dernst <artur.dernst@proton.me>
2025-07-09 00:05:22 +0200

    fixed main for build package

--- chatty_shell/main.py
+++ chatty_shell/main.py
@@ -1,5 +1,10 @@
 from chatty_shell.presenter.presenter import Presenter
 
-if __name__ == "__main__":
+
+def main():
     presenter = Presenter()
     presenter.run()
+
+
+if __name__ == "__main__":
+    main()
